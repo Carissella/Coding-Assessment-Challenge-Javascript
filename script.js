0f3bf7212b39e3cef20a47e902abff19781523b1
Carissella <carissebarrswe@gmail.com>
2023-01-24 18:23:40 -0500

    updated css and added new javascript logic for more quiz questions

--- script.js
+++ script.js
@@ -5,29 +5,61 @@ let answer2 = document.querySelector("#answer2");
 let answer3 = document.querySelector("#answer3");
 let answer4 = document.querySelector("#answer4");
 let scoreBoard = document.getElementById("list");
+let submitButton = document.querySelector("#submitScore");
+let answerbutton = document.querySelectorAll(".ans");
+let timerEl = document.getElementById("timer")
+let startButton = document.createElement("button");
+let rootEl = document.getElementById("root");
+rootEl.appendChild(startButton);
+
+//startButton.addEventListener("click"), function() {
+    
 
 let secondsLeft = 70;
+
+timerEl.textContent = secondsLeft
+
+
 
 let questionsSet = [
     {
         question: "Commonly used data do NOT include:",
-        answers: ["Blah", "Cheese", "Computer Sounds", "All the above"],
+        answers: ["Decimals", "Food", "Computer Sounds", "All the above"],
         correct: "All the above",
-        incorrect: ["Blah", "Cheese", "Computer Sounds",]
+        incorrect: ["Decimals", "Food", "Computer Sounds",]
     },
     {
         question: "Arrays in Javascript can be used to store __",
-        answers: ["Blah", "String Cheese", "Yogurt", "Strings"],
+        answers: ["Functions", "If/Then Statements", "Booleans", "Strings"],
         correct: "Strings",
-        incorrect: ["Blah", "String Cheese", "Yogurt",]
+        incorrect: ["Functions", "If/Then Statements", "Booleans",]
     },
 
     {
         question: "The conditions in an if/then statement is enclosed within __",
-        answers: ["cake", "shrimp", "food poisoning", "curly brackets"],
+        answers: ["Square Brackets", "Parenthesis", "Quotation Marks", "curly brackets"],
         correct: "curly brackets",
-        incorrect: ["cake", "shrimp", "food poisoning",]
-    }
+        incorrect: ["Square Brackets", "Parenthesis", "Quotation Marks",]
+    },
+    {
+        question: "What does the .pop method do to an array?",
+        answers: ["Adds a new value", "Removes a value", "Creates a new array", "Duplicates the array"],
+        correct: "Removes a value",
+        incorrect: ["Adds a new value", "Creates a new array", "Duplicates the array"]
+    },
+    {
+        question: "What does Hyper Text Markup Language abbreviate to?",
+        answers: ["CSS", "SQL", "HTML", "JavaScript"],
+        correct: "HTML",
+        incorrect: ["CSS", "SQL", "JavaScript"]
+    },
+    {
+        question: "What Jquery code gets all elements with the Fish ID?",
+        answers: [""],
+        correct: "",
+        incorrect: [""]
+    },
+
 ];
 
 let timer = setInterval(function () {
@@ -41,11 +73,8 @@ let index = 0;
 
 function newQuestion() {
     let questionIndex = 0;
-    console.log(this.textContent);
-    console.log(questionsSet[questionIndex].correct)
-    console.log(questionIndex)
     if (index < questionsSet.length) {
-        if (this.textContent !== questionsSet[index].correct) {
+        if (this.textContent != questionsSet[index].incorrect) {
             secondsLeft -= 10
         }
         questions.textContent = questionsSet[index].question
@@ -70,7 +99,7 @@ saveScore.addEventListener("click", function () {
     let initials = document.getElementById("initials");
     localStorage.setItem(initials.value, secondsLeft);
     scoreBoard.style = "display: block";
-    document.getElementById("scoreSection").style = "display: none"
+    document.getElementById("scoreSection").style = "display: none";
     for (let i = 0; i < localStorage.length; i++) {
         let li = document.createElement("li");
         li.textContent = localStorage.key(i) + ": " + localStorage.getItem(localStorage.key(i));
@@ -80,10 +109,6 @@ saveScore.addEventListener("click", function () {
     }
 })
 
-let submitButton = document.querySelector("#submitScore");
-
-
-let answerbutton = document.querySelectorAll(".ans");
 
 for (var i = 0; i < answerbutton.length; i++) {
     answerbutton[i].addEventListener("click", newQuestion)
@@ -91,8 +116,6 @@ for (var i = 0; i < answerbutton.length; i++) {
 
 newQuestion();
 
-let timerEl = document.getElementById("timer")
-timerEl.textContent = secondsLeft
 
 
 
